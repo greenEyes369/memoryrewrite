d02b06a831537be32dcd0426e3e56a977314c6e5
greenEyes369 <greenEyes369@github.com>
2024-01-17 03:52:00 -0800

    I figured out how execve works. Basically, you create strings so that you can run your command and pass arguments. Then you declare execve, with the strings you created as arguments so that gets passed into the function and replaces the forked process.

--- memoryrewrite.c
+++ memoryrewrite.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/ptrace.h>
 
 int main() {
     pid_t pid = fork();
@@ -9,9 +10,22 @@ int main() {
         printf("Fork failed: %d\n", errno);
     }
     else if (pid == 0) {
+        char cmd[] = "./clock";
 
+        char * argVec[] = {"clock", NULL};
+        char * envVec[] = {NULL};
+
+        printf("Starting execve call %s:\n", cmd);
+        printf("...\n");
+
+        if(execve(cmd, argVec, envVec) == -1) {
+            perror("Could not execute execve.\n");
+        }
+        printf("Something went wrong...\n");
+
+        return 0;
     }
     else {
-
+        return 0;
     }
 }
